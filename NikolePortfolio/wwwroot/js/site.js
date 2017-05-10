407495fc6de320dd9f0ae692e44e3275c3f204d3
nikolemc <nikolemcstanley@gmail.com>
2017-05-10 15:39:15 -0400

    Portfolio with solcia working and carousel

--- NikolePortfolio/wwwroot/js/site.js
+++ NikolePortfolio/wwwroot/js/site.js
@@ -1,30 +1,8 @@
-﻿//Foundationize this mofo!
-$(document).foundation();
+﻿// Write your Javascript code.
 
-//Touch UI Stuff
-$(function () {
-    var select_delay = 100;
-    if (Modernizr.touch) {
-        //alert('Touch Screen');
-        $("html").addClass("touch-device");
-        $('a').on({
-            'touchstart': function () {
-                $(this).addClass("touched");
-            },
-            'touchend': function () {
-                $(this).removeClass("touched");
-                //After the cool animation, go to the link.
-                window.location.assign($(this).attr("href"));
-            }
-        });
-    } else {
-        $("html").addClass("no-touch");
-    }
-
-    //Make our tasty UI look/work better
-    $("a").on('click', function (e) {
-        //For this demo, disable click
-        e.preventDefault();
-    });
+// Carousel Auto-Cycle
+$(document).ready(function () {
+    $('.carousel').carousel({
+        interval: 6000
+    })
 });
-
